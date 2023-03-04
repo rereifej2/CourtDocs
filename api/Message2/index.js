6f97c6d96f335e7f815c8278680920e0bcfdc7f4
rereifej2 <rania.ereifej@kobrekim.com>
2023-03-03 23:30:10 -0500

    Update index.js

--- api/Message2/index.js
+++ api/Message2/index.js
@@ -1,4 +1,5 @@
-module.exports = async function (context, req) {
+
+    module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
