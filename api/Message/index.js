78819e7be4505cb15a34ba2a583da579b518d964
rereifej2 <rania.ereifej@kobrekim.com>
2023-03-06 21:08:11 -0500

    10

--- api/Message/index.js
+++ api/Message/index.js
@@ -1,30 +1,11 @@
 module.exports = async function (context, req) {
    
    
-   // context.res.json({
-    //    text: "Your IP Address is"
-    //});/
-    context.log('JavaScript HTTP trigger function processed a request.');
-    context.log('Header: ' + req.headers['user-agent']);
+   context.res.json({
+      text: "Your IP Address is"
+  });
 
-    if (true) {
-        context.log(req.headers['Authorization'])
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello there " 
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    body: "Your IP Address is"
-    context.done();
-    
 };
-//};/
 
 
 
